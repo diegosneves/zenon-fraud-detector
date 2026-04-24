e6ac7e34a9a7fe9e83fe7af447c1a0867ff44b7c
Diego Neves <neves.diegoalex@outlook.com>
2026-04-23 23:04:59 -0300

    feat(modelagem de transacoes): :sparkles: Refactor Transaction to use TransactionCustomer and add TransactionFactory for streamlined object creation

--- src/main/java/br/com/zenon/fraud/Transaction.java
+++ src/main/java/br/com/zenon/fraud/Transaction.java
@@ -8,13 +8,30 @@ public record Transaction(
         Integer step,
         TransactionType type,
         BigDecimal amount,
-        String nameOrig,
-        BigDecimal oldBalanceOrig,
-        BigDecimal newBalanceOrig,
-        String nameDest,
-        BigDecimal oldBalanceDest,
-        BigDecimal newBalanceDest,
+        TransactionCustomer origin,
+        TransactionCustomer recipient,
         Boolean isFraud,
         Boolean isFlaggedFraud
 ) {
+
+    public static Transaction create(
+            final Integer step,
+            final TransactionType type,
+            final BigDecimal amount,
+            final TransactionCustomer origin,
+            final TransactionCustomer receiver,
+            final Boolean isFraud,
+            final Boolean isFlaggedFraud
+    ) {
+        return new Transaction(
+                step,
+                type,
+                amount,
+                origin,
+                receiver,
+                isFraud,
+                isFlaggedFraud
+        );
+    }
+
 }
